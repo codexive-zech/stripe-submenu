5f6eba2f753dceabed13ec7895712d976f7ba38e
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-13 11:01:14 +0100

    implemented the display sub link functionality
    iterated over the sublinks array object, then destructed it based on the properties needed started dynamically return the HTML value, then iterated over the destructured link array and destructed it so as to get the needed properties so that it can be used dynamically in the HTML

--- js/stripe.js
+++ js/stripe.js
@@ -21,3 +21,28 @@ toggleBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebarWrapper.classList.remove("show");
 });
+
+// func displaying every sub link in the data array object
+const displaySublinks =
+  // iterating over the sub links so as to it's values and use
+  sublinks
+    .map((item) => {
+      //   destructuring the needed properties of the sub link array object
+      const { page, links } = item;
+      // dynamically display this in the HTML and iterating over the link array object property because it's an array and then destructured it so as to use it's properties in the HTML
+      return `<article>
+    <h4>${page}</h4>
+    <div class="sidebar-sublinks">
+    ${links
+      .map((link) => {
+        const { label, icon, url } = link;
+        return `<a href="${url}"><i class="${icon}"></i>${label}</a>`;
+      })
+      .join("")}
+      </div>
+    </article>`;
+    })
+    .join("");
+
+// dynamically inserting the display sub link functionality into the aside sidebar
+sidebar.innerHTML = displaySublinks;
